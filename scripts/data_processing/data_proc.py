987be8df1a73472adb68bc62a2c53fbffa3c5b54
Yaroslav Dziubenko <76429792+Qizix@users.noreply.github.com>
2025-06-05 08:22:04 +0300

    Update data_proc.py

--- scripts/data_processing/data_proc.py
+++ scripts/data_processing/data_proc.py
@@ -5,7 +5,7 @@ import stanza
 import pymorphy2
 
 
-# Завантаженн стоп-сл
+# Завантаженн стопсл
 def load_stopwords(filepath):
     with open(filepath, 'r', encoding='utf-8') as file:
         return set(word.strip() for word in file.readlines())
